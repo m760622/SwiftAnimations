134f0d10ffb1f8ccefa9e12bac03e0f7dc75cef7
Ananth <agirijak@in.ibm.com>
2018-08-30 21:25:47 +0530

    added custom view

--- CollisionAnimation/CollisionAnimation/UIViewX.swift
+++ CollisionAnimation/CollisionAnimation/UIViewX.swift
@@ -9,13 +9,27 @@
 import UIKit
 
 class UIViewX: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUp()
     }
-    */
-
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setUp() {
+        layer.cornerRadius = frame.width/2
+        backgroundColor = randomColor()
+    }
+    
+    func randomColor() -> UIColor {
+        let red = CGFloat(CGFloat(arc4random()%50000)/50000);
+        let green = CGFloat(CGFloat(arc4random()%50000)/50000);
+        let blue = CGFloat(CGFloat(arc4random()%50000)/50000);
+        
+        return UIColor(red: red, green: green, blue: blue, alpha: 1.0);
+    }
+    
 }
