f6b7c4056281fc7158ebf198a75ffcc205c0e0e0
Ananth <agirijak@in.ibm.com>
2018-09-01 20:34:10 +0530

    Scrolling animation in scrollview

--- CollectionViewLoader/CollectionViewLoader/ViewController.swift
+++ CollectionViewLoader/CollectionViewLoader/ViewController.swift
@@ -8,13 +8,111 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegateFlowLayout {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var lowerFrameY:CGFloat = 0.0
+    var upperFrameY:CGFloat = 0.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        navigationController?.navigationBar.prefersLargeTitles = true
 
+        let layout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 20, left: 15, bottom: 15, right: 15)
+        layout.minimumLineSpacing = 5.0
+        layout.minimumInteritemSpacing = 5.0
+        collectionView.collectionViewLayout = layout
+        
+        lowerFrameY = collectionView.bounds.height * (2/3)
+        upperFrameY = view.bounds.height * (1/5)
+    }
 
 }
 
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath)
+       animateCell(cell)
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = (self.view.bounds.width / 2) - 20
+        return CGSize(width: width, height: width + 40)
+    }
+    
+}
+
+extension ViewController {
+    
+    
+    func animateCell(_ cell : UICollectionViewCell) {
+        
+        let cellLocation = cell.convert(cell.bounds, to: view)
+        
+        if cellLocation.minY >= lowerFrameY {
+            animateBottomScaling(cell)
+        } else if (cellLocation.minY <= upperFrameY) {
+            animateTopScaling(cell)
+        } else {
+            cell.transform = CGAffineTransform(scaleX: 1, y: 1)
+        }
+        
+    }
+    
+    func animateBottomScaling(_ cell:UICollectionViewCell) {
+        
+        let cellLocation = cell.convert(cell.bounds, to: view)
+        let differenceY = (cellLocation.minY - lowerFrameY) / (collectionView.bounds.height - lowerFrameY)
+        let scaleRawValue = getScaleValue(differenceY)
+        
+        let scaleValue = scaleRawValue * 0.2
+        
+        cell.transform = CGAffineTransform(scaleX: 1-scaleValue, y: 1-scaleValue)
+        
+    }
+    
+    
+    func animateTopScaling(_ cell:UICollectionViewCell) {
+        
+        let cellLocation = cell.convert(cell.bounds, to: view)
+        
+        let differenceY = (upperFrameY - cellLocation.minY) / (collectionView.bounds.height - upperFrameY)
+        let scaleRawValue = getScaleValue(differenceY)
+        
+        let scaleValue = scaleRawValue * 0.2
+        
+        cell.transform = CGAffineTransform(scaleX: 1 - scaleValue, y: 1 - scaleValue)
+        
+    }
+    
+    func getScaleValue(_ value: CGFloat) -> CGFloat {
+        
+        if value < 0 {
+            return 0
+        } else if value > 1 {
+            return 1
+        }
+        return value
+    }
+    
+    
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        collectionView.visibleCells.forEach { (cell) in
+            animateCell(cell)
+        }
+    }
+    
+    
+}
