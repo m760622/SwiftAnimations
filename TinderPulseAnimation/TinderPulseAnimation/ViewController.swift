014ce2ce839a536f5da81a9c7dac958fc70358a3
Ananth <agirijak@in.ibm.com>
2018-09-06 02:06:59 +0530

    Class change

--- TinderPulseAnimation/TinderPulseAnimation/ViewController.swift
+++ TinderPulseAnimation/TinderPulseAnimation/ViewController.swift
@@ -10,11 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var segmentedController: UISegmentedControl!
+    @IBOutlet var searchingImageView: TinderSearchImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        searchingImageView.initRippleAnimation()
     }
 
-
+    @IBAction func selectionAction(_ sender: UISegmentedControl) {
+        
+        
+        switch sender.selectedSegmentIndex {
+        case 0:
+            
+            segmentedController.setImage(UIImage(named: "Group 1")?.withRenderingMode(UIImage.RenderingMode.alwaysOriginal), forSegmentAt: 0)
+            segmentedController.setImage(UIImage(named: "Group 4")?.withRenderingMode(UIImage.RenderingMode.alwaysOriginal), forSegmentAt: 1)
+            break
+        case 1:
+            segmentedController.setImage(UIImage(named: "Group 2")?.withRenderingMode(UIImage.RenderingMode.alwaysOriginal), forSegmentAt: 0)
+            segmentedController.setImage(UIImage(named: "Group 3")?.withRenderingMode(UIImage.RenderingMode.alwaysOriginal), forSegmentAt: 1)
+            break
+        default:
+            print("no actions")
+        }
+    }
+    
 }
 
