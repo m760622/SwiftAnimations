721f444911f620bb8dc1cc947191360b00a77b50
Ananth <agirijak@in.ibm.com>
2018-09-06 02:06:39 +0530

    image file edit

---
+++ TinderPulseAnimation/TinderPulseAnimation/TinderSearchImageView.swift
@@ -0,0 +1,21 @@
+//
+//  TinderSearchImageView.swift
+//  TinderPulseAnimation
+//
+//  Created by Anantha Krishnan K G on 06/09/18.
+//  Copyright © 2018 Ananth. All rights reserved.
+//
+
+import UIKit
+
+class TinderSearchImageView: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
