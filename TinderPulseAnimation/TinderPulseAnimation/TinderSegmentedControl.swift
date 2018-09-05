d4cfebeab347ab83fc7784ab3db5e32814d77f13
Ananth <agirijak@in.ibm.com>
2018-09-05 23:48:29 +0530

    added project files

---
+++ TinderPulseAnimation/TinderPulseAnimation/TinderSegmentedControl.swift
@@ -0,0 +1,21 @@
+//
+//  TinderSegmentedControl.swift
+//  TinderPulseAnimation
+//
+//  Created by Anantha Krishnan K G on 05/09/18.
+//  Copyright © 2018 Ananth. All rights reserved.
+//
+
+import UIKit
+
+class TinderSegmentedControl: UIControl {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ TinderPulseAnimation/TinderPulseAnimation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TinderPulseAnimation
+//
+//  Created by Anantha Krishnan K G on 05/09/18.
+//  Copyright © 2018 Ananth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
