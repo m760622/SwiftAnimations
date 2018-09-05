cf04aac0008f9090592406752365ec42f136e183
Ananth <agirijak@in.ibm.com>
2018-09-06 02:07:18 +0530

    custom segmented view

--- TinderPulseAnimation/TinderPulseAnimation/TinderSegmentedControl.swift
+++ TinderPulseAnimation/TinderPulseAnimation/TinderSegmentedControl.swift
@@ -10,19 +10,6 @@ import UIKit
 
 @IBDesignable class TinderSegmentedControl: UISegmentedControl {
 
- 
-    @IBInspectable public var borderColor: UIColor = UIColor.darkGray {
-        didSet {
-            self.layer.borderColor = borderColor.cgColor
-            self.layer.borderWidth = 1.5
-        }
-    }
-    
-    @IBInspectable public var borderWidth: CGFloat = 2.0 {
-        didSet {
-            self.layer.borderWidth = borderWidth
-        }
-    }
     @IBInspectable public var cornerRadius: CGFloat = 2.0 {
         didSet {
             self.layer.cornerRadius = cornerRadius
