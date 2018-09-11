c3ef6d37e0cd18e29bec794eb810751395571a8a
Ananth <agirijak@in.ibm.com>
2018-09-11 22:59:47 +0530

    Added circular transition

---
+++ CircularViewTransition/CircularViewTransition/Transition/TransitionClass.swift
@@ -0,0 +1,13 @@
+//
+//  TransitionClass.swift
+//  CircularViewTransition
+//
+//  Created by Anantha Krishnan K G on 11/09/18.
+//  Copyright © 2018 Ananth. All rights reserved.
+//
+
+import UIKit
+
+class TransitionClass: NSObject {
+
+}
---
+++ CircularViewTransition/CircularViewTransition/ViewControllerTwo.swift
@@ -0,0 +1,30 @@
+//
+//  ViewControllerTwo.swift
+//  CircularViewTransition
+//
+//  Created by Anantha Krishnan K G on 11/09/18.
+//  Copyright © 2018 Ananth. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerTwo: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
