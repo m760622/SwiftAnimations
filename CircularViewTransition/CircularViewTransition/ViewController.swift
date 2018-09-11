612fa82e0dbcfe1967307ca96482ee22fdb9f55e
Ananth <agirijak@in.ibm.com>
2018-09-11 23:00:37 +0530

    Viewcontrollers

--- CircularViewTransition/CircularViewTransition/ViewController.swift
+++ CircularViewTransition/CircularViewTransition/ViewController.swift
@@ -8,13 +8,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIViewControllerTransitioningDelegate {
 
+    let transition = TransitionClass()
+
+    
+    @IBOutlet var menuButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        menuButton.setShadow()
     }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let secondVC = segue.destination as! ViewControllerTwo
+        secondVC.transitioningDelegate = self
+        secondVC.modalPresentationStyle = .custom
+    }
+    
+    func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        transition.transitionMode = .present
+        transition.startingPoint = menuButton.center
+        transition.circleColor = menuButton.backgroundColor!
+        transition.button = menuButton
+        
+        return transition
+    }
+    
+    func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        transition.transitionMode = .dismiss
+        transition.startingPoint = menuButton.center
+        transition.circleColor = menuButton.backgroundColor!
+        transition.button = menuButton
+        
+        return transition
+    }
 
 }
 
+extension UIButton {
+    
+     func setShadow() {
+        self.layer.shadowColor = UIColor.black.cgColor
+        self.layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
+        self.layer.masksToBounds = false
+        self.layer.shadowRadius = 1.0
+        self.layer.shadowOpacity = 0.5
+    }
+}
--- CircularViewTransition/CircularViewTransition/ViewControllerTwo.swift
+++ CircularViewTransition/CircularViewTransition/ViewControllerTwo.swift
@@ -10,21 +10,17 @@ import UIKit
 
 class ViewControllerTwo: UIViewController {
 
+    @IBOutlet var closeButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        closeButton.setShadow()
+        
         // Do any additional setup after loading the view.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func closeMenu(_ sender: Any) {
+        
+        dismiss(animated: true, completion: nil)
     }
-    */
-
 }
