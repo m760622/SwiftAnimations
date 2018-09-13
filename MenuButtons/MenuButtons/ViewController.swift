bf3b27578e94e08ae39b655c8398e39f4ba92ece
Ananth <agirijak@in.ibm.com>
2018-09-13 23:44:56 +0530

    Menu actions

---
+++ MenuButtons/MenuButtons/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MenuButtons
+//
+//  Created by Anantha Krishnan K G on 13/09/18.
+//  Copyright © 2018 Ananth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
